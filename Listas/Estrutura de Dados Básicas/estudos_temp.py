47cd94ea00858f0e8f77de925c61bddf265c61f7
Dionilton <dionilton.os@gmail.com>
2022-01-28 17:47:21 -0300

    ajustes

--- Listas/Estrutura de Dados Básicas/estudos_temp.py
+++ Listas/Estrutura de Dados Básicas/estudos_temp.py
@@ -21,14 +21,14 @@ x = input()
 x = list(x)
 ope = "+-*/"
 let = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-NUM = "0123456789"
+num = "0123456789"
 saida = ""
 par_stack = Stack()
 ope_stack = Stack()
 
 ind = 0
 for i in x:
-    if i in let:
+    if i in let i in num:
         saida += i
     elif i == "(":
         par_stack.push(i)
