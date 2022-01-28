330626315edb051eb897e33032db5f7973ac2317
Dionilton <dionilton.os@gmail.com>
2022-01-28 17:27:19 -0300

    salvando

--- Listas/Estrutura de Dados Básicas/estudos_temp.py
+++ Listas/Estrutura de Dados Básicas/estudos_temp.py
@@ -12,30 +12,30 @@ class Stack:
         return self.items.pop()
     
     def peek(self):
-        return self.items[len(self.items) - 1]
+        return self.items[len(sel.items) - 1]
     
     def size(self):
         return len(self.items)
     
-def parChecker(string):
-    s = Stack()
-    balanced = True
-    index = 0
-    while index < len(string) and balanced:
-        simbolo = string[index]
-        if simbolo == '(':
-            s.push(simbolo)
-        else:
-            if s.isEmpty():
-                balanced = False
-            else:
-                s.pop()
-        index += 1
-        
-        if balanced and s.isEmpty():
-            return True
-        else:
-            return False
-        
-print(parChecker('((()))'))
-print(parChecker('(()'))
+x = input()
+x = list(x)
+ope = "+-*/"
+let = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+NUM = "0123456789"
+
+par_stack = Stack()
+ope_stack = Stack()
+
+ind = 0
+for i in x:
+    if i == "(":
+        par_stack.push(i)
+    if i in ope:
+        ope_stack.push(i)
+    if i == ")":
+        ope_stack.pop()
+        par_stack.pop()
+        x[ind] = ope_stack.pop()
+    ind += 1
+
+print(x)
