7cffc8761f5924cafd24b61cf26838a975ddd4c6
Dionilton <dionilton.os@gmail.com>
2022-02-01 23:05:05 -0300

    Q04

--- Listas/Estrutura de Dados Básicas/03_real_life_ligthsaber_combat_academy.py
+++ Listas/Estrutura de Dados Básicas/03_real_life_ligthsaber_combat_academy.py
@@ -1,16 +1,17 @@
 def exibe_candidatos(deque, pos, ordem):
-    if ordem == 'direta':
-        for i in range(deque.size()):
-            if i < pos:
-                deque.remove_front()
-            else:
-                print(deque.remove_front())
-    elif ordem == 'inversa':
-        ind = deque.size()
-        
-        for i in range(deque.size()):
-            if ind > pos:
-                deque.remove_rear()
-            else:
-                print(deque.remove_rear())
-            ind -= 1
+    if pos < deque.size() and pos >= 0:
+        if ordem == 'direta':
+            for i in range(deque.size()):
+                if i < pos:
+                    deque.remove_front()
+                else:
+                    print(deque.remove_front())
+        elif ordem == 'inversa':
+            ind = deque.size()
+            
+            for i in range(deque.size()):
+                if ind > pos + 1:
+                    deque.remove_rear()
+                else:
+                    print(deque.remove_rear())
+                ind -= 1
---
+++ Listas/Estrutura de Dados Básicas/04_anilhas_de_academia.py
@@ -0,0 +1,42 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+        
+    def isEmpty(self):
+        return self.items == []
+    
+    def push(self, item):
+        self.items.append(item)
+        
+    def pop(self):
+        return self.items.pop()
+        
+    def peek(self):
+        return self.items[len(self.items) - 1]
+    
+    def size():
+        return len(self.items)
+    
+s = Stack()
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    else:
+        s.push(n)
+
+retiradas = 0
+total_peso = 0
+
+a = int(input())
+
+while True:
+    r = int(s.pop())
+    retiradas += 1
+    total_peso += r
+    print(f'Peso retirado: {r}')
+    if r == a:
+        break
+print(f'Anilhas retiradas: {retiradas}')
+print(f'Peso total movimentado: {total_peso}')
