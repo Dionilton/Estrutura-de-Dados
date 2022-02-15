9ecff2a3d383820487b006e4a5b6abadd55e7745
Dionilton <dionilton.os@gmail.com>
2022-02-15 13:25:01 -0300

    atualização Q09

---
+++ Listas/Estrutura de Dados Básicas/09_janela_deslizante.py
@@ -0,0 +1,42 @@
+class Deque:
+    def __init__(self):
+        self.items = []
+        
+    def isEmpty(self):
+        return self.items == []
+    
+    def addFront(self, item):
+        self.items.append(item)
+        
+    def addRear(self, item):
+        self.items.insert(0, item)
+    
+    def removeFront(self):
+        return self.items.pop()
+    
+    def removeRear(self):
+        return self.items.pop(0)
+    
+    def size(self):
+        return len(self.items)
+    
+n = int(input())
+d = Deque()
+janela = input().split()
+k = int(input())
+saida = []
+
+
+for i in range(n):
+    janela[i] = int(janela[i])
+  
+max = 0
+for i in range(len(janela)):
+    if i < k:
+        d.addFront(janela[i])
+        if janela[i] > max
+        max = janela[i]
+        if i == k - 1:
+            saida.append(max)
+
+
