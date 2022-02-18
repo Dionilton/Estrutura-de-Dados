7de2b1172f14f92aa6732fdd2f18c57f2241b924
Dionilton <dionilton.os@gmail.com>
2022-02-18 14:03:16 -0300

    Q08

---
+++ Listas/Estrutura de Dados Básicas/08_2_palindromo.py
@@ -0,0 +1,8 @@
+"""
+Dar um split na entrada
+depois faz um for interando a lista do split
+analisa item por item
+se a palavrar for 2-palíndromo (parte mais "difícil", validar se a palavra é 2-palíndromo)
+retorna/print a palavra
+
+"""
