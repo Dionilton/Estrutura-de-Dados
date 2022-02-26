7c4110ba3fbaae5cbaf85a626363a9f6bdfadccc
Dionilton <dionilton.os@gmail.com>
2022-02-26 20:17:12 -0300

    lista ordenada

--- Listas/Estrutura de Dados Básicas/lista_encadeada_ordenada.py
+++ Listas/Estrutura de Dados Básicas/lista_encadeada_ordenada.py
@@ -6,7 +6,7 @@ class Node:
     def getData(self):
         return self.data
     
-    def getNexr(self):
+    def getNext(self):
         return self.next
     
     def setData(self, newdata):
@@ -32,5 +32,83 @@ class OrderedList:
     def isEmpty(self):
         return self.head == None
     
+    def search(self, item):
+        current = self.head
+        found = False
+        stop = False
+        while current != None and not found and not stop:
+            if current.getData() == item:
+                found = True
+            else:
+                if current.getData() > item:
+                    stop = True
+                else:
+                    current = current.getNext()
+        
+        return found
     
+    def add(self, item):
+        current = self.head
+        previous = None
+        stop = False
+        while current != None and not stop:
+            if current.getData() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.getNext()
+                
+        temp = Node(item)
+        if previous == None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            temp.setNext(current)
+            previous.setNext(temp)
+            
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+            
+        return count
+    
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+                
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+        
+        
+L = OrderedList()
+L.add(5)
+L.add(8)
+L.add(2)
+L.add(6)
+print(L)
+L.remove(6)
+print(L)
+print(L.size())
+print(L.search(8))
+print(L.search(2))
+print(L.search(18))
+print(L.isEmpty())
+L.remove(8)
+L.remove(2)
+print(L)
+L.remove(5)
+print(L.isEmpty())
+
     
