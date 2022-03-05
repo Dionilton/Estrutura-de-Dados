38fb92007aa1171c4935adc06b9fbddb8e41fbcf
Dionilton <dionilton.os@gmail.com>
2022-03-04 21:59:18 -0300

    cmt

--- Trabalhos/t1_santa_concorrencia.py
+++ Trabalhos/t1_santa_concorrencia.py
@@ -38,7 +38,38 @@ class Stack:
     
     def size(self):
         return len(self.items)
+    
+#Implementação da classe Node
 
+class Node:
+    
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+        
+    def getData(self):
+        return self.data
+    
+    def getNext(self):
+        return self.next
+    
+    def setData(self, newdata):
+        self.data = newdata
+    
+    def setNext(self, newnext):
+        self.next = newnext
+
+#Implementação do tipo abtrato Lista Desordenada
+        
+class UnorderedList:
+    
+    def __ini__(self):
+        self.head = None
+        
+    def isEmpty(self):
+        return self.head = None
+    
+    
 
 #Funções
 
