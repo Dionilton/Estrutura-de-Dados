cbbe60dd5adbdc94e96bfbd5ddcacc321bc15a5f
Dionilton <dionilton.os@gmail.com>
2022-03-10 12:59:45 -0300

    Q6

--- Listas/Recursão/06_fibonacci_2.py
+++ Listas/Recursão/06_fibonacci_2.py
@@ -1,4 +1,15 @@
-lista = []
 def fibonacci(n):
-    global lista
+    lista = []
+    for i in range(n):
+        lista.append(fib(i))
     
+    return lista
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+    
+print(fibonacci(19))
