0951947f1a98afcbb5c13ae4410a66650c3a39a9
Dionilton <dionilton.os@gmail.com>
2022-03-31 11:04:44 -0300

    cmt

--- Listas/Árvores/arvore_binaria_busca.py
+++ Listas/Árvores/arvore_binaria_busca.py
@@ -2,12 +2,16 @@ def arvoreBusca(lista):
     arvore = Node(lista[0])
     current = arvore
     for i in range(1, len(lista)):
+        temp = Node(lista[i])
         while current.getRigth() != None or current.getLeft() != None:
             if lista[i] > current.getData():
                 current = current.getRigth()
             else:
-                
-            
+                current = current.getLeft()
+        if lista[i] > current.getData():
+            current.setRigth(temp)
+        elif lista[i] < current.GetData():
+            current.setLeft(temp)
     
 class Node:
     def __init__(self, initdata):
