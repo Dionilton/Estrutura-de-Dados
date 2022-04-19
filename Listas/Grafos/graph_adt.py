f08cc0c65c25d8226f3a4c0e2531c28899498cdc
Dionilton <dionilton.os@gmail.com>
2022-04-19 12:31:52 -0300

    class Graph

--- Listas/Grafos/graph_adt.py
+++ Listas/Grafos/graph_adt.py
@@ -1,4 +1,5 @@
 class Vertex:
+    
     def __init__(self, key):
         self.id = key
         self.connectedTo = {}
@@ -17,3 +18,39 @@ class Vertex:
     
     def getWeigth(self, nbr):
         return self.connectedTo[nbr]
+    
+
+class Graph:
+    
+    def __init__(self):
+        self.vertList = {}
+        self.numVertices = 0
+        
+    def addVertex(self, key):
+        self.numVertices += 1
+        newVertice = Vertex(key)
+        self.vertList[key] = newVertex
+        return newVertex
+    
+    def getVertex(self, n):
+        if n in self.verticeList:
+            return self.verticeList[n]
+        else:
+            return None
+        
+    def __contains__(self, n):
+        return n in self.vertList
+    
+    def addEdge(self, f, t, cost=0):
+        if f not in self.vertList:
+            nv = self.addVertex(f)
+        if t not in self.vertList:
+            nv = self.addVertex(t)
+        self.vertList[f].addNeighbor(self.vertList[t], cost)
+        
+    def getVertices(self):
+        return self.vertList.keys()
+    
+    def __iter__(self):
+        return iter(self.vertList.values())
+        
