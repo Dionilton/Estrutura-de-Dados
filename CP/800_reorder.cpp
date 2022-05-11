ecf40e440b08c1ac8e9f70aad14014cb87ad5fbd
Dionilton <dionilton.os@gmail.com>
2022-05-11 13:51:26 -0300

    cmt

--- CP/800_reorder.cpp
+++ CP/800_reorder.cpp
@@ -3,7 +3,17 @@ using namespace std;
 
 int main(){
 
-    
+    int x;
+    cin >> x;
+    int n,m;
+    for(int i = 0; i<x; i++){
+        cin >> n >> m >> endl;
+        int vec[n];
+        for(int j=0; j<n; j++){
+            cout << vec[j]
+        }
+        
+    }
 
     return 0;
 }
