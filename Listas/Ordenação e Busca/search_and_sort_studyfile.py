372f795bcf800c79650fde3ba043486ea62b83e0
Dionilton <dionilton.os@gmail.com>
2022-06-24 10:26:39 -0300

    initial structure

---
+++ Listas/Ordenação e Busca/search_and_sort_studyfile.py
@@ -0,0 +1,31 @@
+#SEARCH:
+
+
+#Sequential Search
+
+
+#Binary Search
+
+
+#SORT:
+
+
+#Selection Sort
+
+
+#Bubble Sort
+
+
+#Merge Sort
+
+
+#Quick Sort
+
+
+#Insertion Sort
+
+
+#Shell Sort
+
+
+#Hashing
