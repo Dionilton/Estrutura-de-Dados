9ff7cfcc0331944e0845c81df6f872bd78def901
Dionilton <dionilton.os@gmail.com>
2022-06-24 11:12:52 -0300

    sequential search function

--- Listas/Ordenação e Busca/search_and_sort_studyfile.py
+++ Listas/Ordenação e Busca/search_and_sort_studyfile.py
@@ -3,7 +3,18 @@
 
 #Sequential Search
 
-
+def sequentialSearch(lista, item):
+    index = 0
+    found = False
+    
+    while index < len(lista) and not found:
+        if lista[index] == item:
+            found = True
+        else:
+            index += 1
+            
+    return found
+    
 #Binary Search
 
 
@@ -29,3 +40,11 @@
 
 
 #HASHING:
+
+
+#Space to tests cases:
+
+testlist = [1, 2, 32, 8, 17, 19, 42, 13, 0]
+print(sequentialSearch(testlist, 3))
+print(sequentialSearch(testlist, 13))
+
